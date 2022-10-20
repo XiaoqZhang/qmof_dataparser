4da4488bd7d6aac2c26390b8f87ec598e6399501
XiaoqZhang <xiaoqi.zhang@epfl.ch>
2022-10-20 19:59:14 +0200

    change bash script to serial

--- bash_scripts/run_procar_parser.sh
+++ bash_scripts/run_procar_parser.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #SBATCH --nodes 1
-#SBATCH --ntasks-per-node 8
+#SBATCH --ntasks-per-node 1
 #SBATCH --cpus-per-task 1
+#SBATCH --partition serial
 #SBATCH --mem 32000
 #SBATCH --time 48:00:00
 
