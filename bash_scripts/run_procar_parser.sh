49b7ee6cf07173b11db146548f3f87e8e0343964
XiaoqZhang <xiaoqi.zhang@epfl.ch>
2022-10-20 19:55:24 +0200

    modify submission script

--- bash_scripts/run_procar_parser.sh
+++ bash_scripts/run_procar_parser.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-#SBATCH --nodes 4
-#SBATCH --ntasks-per-node 16
+#SBATCH --nodes 1
+#SBATCH --ntasks-per-node 8
 #SBATCH --cpus-per-task 1
 #SBATCH --mem 32000
 #SBATCH --time 48:00:00
 
 module purge
-module load mycompiler
-module load mympi
 
 srun /home/xiazhang/anaconda3/envs/ml/bin/python ../procar_parser.py
